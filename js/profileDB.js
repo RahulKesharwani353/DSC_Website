0692d5b4de486be7611ed77051ebe7e6a212fe70
Rahul Kesharwani <rahulkesharwani353@gmail.com>
2021-03-17 21:18:36 +0530

    database connected for signup

---
+++ js/profileDB.js
@@ -0,0 +1,38 @@
+document.getElementById("submit_btn").onclick = function(){
+
+    var fName = document.getElementById("fname").value;
+    var lName = document.getElementById("lname").value;
+    var phn = document.getElementById("phno").value;
+    var phn_2 = document.getElementById("phno_2").value;
+    var course = document.getElementById("course").value;
+    var branch = document.getElementById("branch").value;
+    var colle = document.getElementById("college").value;
+    var passYr = document.getElementById("passYear").value;
+   
+    if(fName==""||lName==""||phn==""||course==""||branch==""||colle==""||passYr==""){
+      alert("Enter All The Required Details");
+      location.reload();
+    }
+    else{
+      var user =firebase.auth().currentUser;
+    var UID = firebase.auth().currentUser.uid;
+
+    var database = firebase.database();
+      firebase.database().ref("members/"+UID).set({
+        email: user.email,
+        name: fName+" "+lName,
+        phone: phn,
+        altPhone: phn_2,
+        course: course,
+        branch: branch,
+        college: colle,
+        passYear: passYr,
+        UID: UID
+
+      }).then(() => {
+        alert("Welcome");
+        window.location.href ="profile.html";
+    });
+    }
+     
+   }
